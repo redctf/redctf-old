2317ae218811671f84eb0428012b60e1ab66b832
clevernyyyy <adam.t.schaal@gmail.com>
2017-12-19 23:37:13 -0600

    setting up first django connection

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,77 @@
 import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 import { Redirect } from "react-router-dom";
+import axios from "axios";
 
 @inject("store")
 @observer
 export default class Login extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	onSubmit(event) {
+		const port = 8000;
+		axios.defaults.baseURL = `${location.protocol}//${location.hostname}:${port}`;
+		const mutation = this.postLogin("ryan", "abcd1234");
+		axios.post('/graphql/',
+			{
+				query: mutation,
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+				},
+			}
+	  )
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+	}
+
+	postLogin(username, password) {
+		return `mutation { login ( username: "${username}", password: "${password}") {status} }`;
+	}
+
+
+	whoami() {
+		const port = 8000;
+		axios.defaults.baseURL = `${location.protocol}//${location.hostname}:${port}`;
+		axios.post('/graphql/',
+			{
+				query: "query{me}",
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+				},
+			}
+	  )
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+	}
+
 	render() {
 		return (
 			<div className="page login">
-				Your login form here...
+				
+				<form>
+					<input type="text" placeholder="name"/>
+					<input type="text" placeholder="password"/>
+				</form>
+				<button type="button" onClick={this.onSubmit.bind(this)}>Login</button>
+
+
+				<button type="button" onClick={this.whoami.bind(this)}>whoami</button>
+
+
+
+
 				{this.props.store.authenticated &&
 					!this.props.store.authenticating &&
 					<Redirect to="/" />}
--- server/redctf/settings.py
+++ server/redctf/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'graphene_django',
     'challenges',
     'users',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,5 @@ GRAPHENE = {
 
 AUTH_USER_MODEL = 'users.User'
 
+CORS_ORIGIN_WHITELIST = ( 'localhost:3000', 'localhost:8000', )
+CORS_ALLOW_CREDENTIALS=True
