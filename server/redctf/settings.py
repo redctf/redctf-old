400f61f13548c07985df47f58983de7829116731
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2017-12-26 23:02:19 -0600

    Add teams model

--- server/redctf/settings.py
+++ server/redctf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'graphene_django',
     'challenges',
     'users',
+    'teams',
     'corsheaders'
 ]
 
--- server/users/models.py
+++ server/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from teams.models import Team
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     verified = models.BooleanField(default=False)
+    team = models.ForeignKey(Team, default=None, null=True, on_delete=models.SET_NULL)
