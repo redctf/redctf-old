f873fec6cb0d707dca05ffc66d34f7c8acbe3cc8
Nate Wood <nwood01@unomaha.edu>
2019-06-20 13:11:29 -0500

    fixed createContainer container name code, updated args on test script

--- dockerAPI/createContainer.py
+++ dockerAPI/createContainer.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Get Docker Service object by ID.')
 parser.add_argument('user', metavar='username', help='The username to add the container to.')
 parser.add_argument('imageName', metavar='image name', help='The image and version to use.')
 parser.add_argument('ports', metavar='ports', help='Must use port#/protocol:hostport (hostport can be None) - this is a dict')
-parser.add_argument('--pathPrefix', metavar='pathPrefix', help='Path prefix for traefik reverse proxy: \'/hello\' is an example')
+parser.add_argument('--pathPrefix', metavar='pathPrefix', help='Path prefix for traefik reverse proxy: \'hello\' is an example - don\'t use leading forward slash.')
 
 args = parser.parse_args()
 # port format - for static argument.
