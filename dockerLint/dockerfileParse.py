07866a94d811b6c0c4ae02368340f314ba61047a
woodnsec <woodnsec@gmail.com>
2020-01-08 19:15:47 +0000

    added debug flag to lintDockerFile

--- dockerLint/dockerfileParse.py
+++ dockerLint/dockerfileParse.py
@@ -14,92 +14,73 @@ class dockerAPI:
         self.client = d.from_env()
 
     def parseDockerfile(self, dockerfile):
-        with open(file=dockerfile) as f:
-            r = DockerfileParser(fileobj=f)
-            r_dict = {}
-            # r_dict['baseimage'] = r.baseimage
-            # r_dict['cache_content'] = r.cache_content
-            # r_dict['cached_content'] = r.cached_content
-            # r_dict['cmd'] = r.cmd
-            # r_dict['content'] = r.content
-            r_dict['labels'] = r.labels
+        try:
+            with open(file=dockerfile) as f:
+                r = DockerfileParser(fileobj=f)
+                r_dict = {}
+                # uncomment below to add additional fields from parser object to dictionary. 
+                # r_dict['baseimage'] = r.baseimage
+                # r_dict['cache_content'] = r.cache_content
+                # r_dict['cached_content'] = r.cached_content
+                # r_dict['cmd'] = r.cmd
+                # r_dict['content'] = r.content
+                r_dict['labels'] = r.labels
 
-            r_ports = []
-            count = 0
-            for line in r.structure:
-                if line['instruction'] == 'EXPOSE':
-                    # print("PORT: {0}".format(line['value']))
-                    r_ports.append(line['value'])
+                r_ports = []
+                count = 0
+                for line in r.structure:
+                    if line['instruction'] == 'EXPOSE':
+                        r_ports.append(line['value'])
 
-            r_dict['ports'] = r_ports
+                r_dict['ports'] = r_ports
 
-            r_json = json.dumps(r_dict)
+                r_json = json.dumps(r_dict)
 
-            return r_json
-
-    # def buildLinterContainer(self):
-    #     imageName = 'dft'
-    #     cmd = '/bin/bash'
-
-    #     linterContainerObject = self.client.containers.create(
-    #         imageName, detach=True, name='redctf-dockerfile-linter', auto_remove=False, privileged=True, volumes={'/': {'bind': '/root/', 'mode': 'rw'}}, command=cmd)
-    #     # linterContainerObject.run()
-    #     """docker run -it 
-    #     --rm --privileged 
-    #     -v $PWD:/root/
-    #     projectatomic/dockerfile-lint 
-    #     dockerfile_lint -p -f dockerfile -r rules.yaml -j"""
-
-    #     return linterContainerObject
-
-#     def lintDockerfile(self, dockerfile, rulesFile, path):
-#         """
-#         Lint a dockerfile .
-#         :from: https://docker-py.readthedocs.io/en/stable/containers.html
-#         :param dockerfile: fully qualified path to dockerfile
-#         :param rulesFile: fully qualified path to rules.yaml file
-#         :return: docker host response
-#         """
-#         # run the container and return output
-#         linter = 'dft'
-#         commands = "dockerfile_lint -p -f {0} -r {1} -j".format(dockerfile, rulesFile)
-#         # commands = "dockerfile_lint" # -p -f {0} -r {1} -j".format(dockerfile, rulesFile)
-#         # print("commands:{0}".format(commands))
-#         try:
-#             container = self.client.containers.run(image=linter, auto_remove=False, privileged=True, volumes={
-# path: {'bind': '/root/', 'mode': 'rw'}},  detach=True,  stderr=True, stdout=True, stream=False, command = commands)
-
-#         except Exception as ex:
-#             print(ex)
-
-#         return 
-
-    def lintDockerFile(self, dockerfile, rulesFile): #TODO: write debug flag and pass output if debug - otherwise status code. 
-        container = self.getContainerObject('dft-test')
-        commands = "dockerfile_lint -p -f {0} -r {1} -j".format('dockerfile', './rules.yaml')
-        # TODO: build the stupid container in the docker-compose file. 
-        if not container:
-            container = self.client.containers.create('dft', name='dft-test',  detach=False, auto_remove=False, command='sleep 1000', volumes={
- '/home/nate/redctf/dockerLint': {'bind': '/dockerLint', 'mode': 'rw'}}, privileged=True)
-            # print(type(container))
-
-            # container.start()
+                return r_json
             
-        containerDict  = {}
-        containerDict['status'] = container.status
-        if containerDict['status'] == 'created':
-            container.start()
-        # pwd = container.exec_run('pwd', stream=False, demux=False)
-        # for line in pwd:
-        #     print(line)
+        except Exception as ex:
+            raise ex
+
+    def lintDockerFile(self, dockerfile, rulesFile, volume, debug=False):
+        
+        commands = "dockerfile_lint -p -f {0} -r {1} -j".format(
+            dockerfile, rulesFile)
+
+        if debug == False:
+
+            try:
+                container = self.client.containers.run('dft', command = commands, auto_remove = True, volumes = {
+                                                   volume: {'bind': '/dockerLint', 'mode': 'rw'}}, privileged=True)
+                return container
+            except Exception as ex:
+                raise ex
+        
+        elif debug == True:
             
-        log = container.exec_run(commands, stream=False, demux=False)
-        container.kill()
-        for line in log: 
-            print(line,end='')
-        print(container.status)
-        container.remove()
-        return log
+            try:
+                container = self.getContainerObject('dft-test')
+                if not container:
+                    container = self.client.containers.create('dft', name='dft-test',  detach=False, auto_remove=False, command='sleep 1000', volumes={
+                                                            volume: {'bind': '/dockerLint', 'mode': 'rw'}}, privileged=True)
+
+                containerDict = {}
+                containerDict['status'] = container.status
+                if containerDict['status'] == 'created':
+                    container.start()
+
+                log = container.exec_run(commands, stream=False, demux=False)
+                container.kill()
+                for line in log:
+                    print(line, end='')
+                print(container.status)
+                container.remove()
+                return log
+            except Exception as ex:
+                raise ex
+
+        else:
+            raise("Unknown debug flag")
+            
 
     def getContainerObject(self, containerName):
         """
@@ -110,7 +91,6 @@ class dockerAPI:
         """
         try:
             containerObject = self.client.containers.get(containerName)
-            # print(containerObject)
 
         except Exception as ex:
             print(ex)
--- dockerLint/parseDockerfile.py
+++ dockerLint/parseDockerfile.py
@@ -5,24 +5,37 @@ from dockerfileParse import dockerAPI
 d = dockerAPI()
 
 pp = pprint.PrettyPrinter(indent=4)
-
+retcode = 0
 
 path = "/"
-filename = "./dockerLint/dockerfile"
-rulesfile = "./dockerLint/rules.yaml"
-print("##################")
+filename = "./dockerLint/dockerfile-test"
+dockerfile = 'dockerfile-test'
+rulesfile = "rules.yaml"
+volume = '/home/nate/redctf/dockerLint'
+debug = True
+print("##################\nJSON:")
 
 r = d.parseDockerfile(filename)
 
 pp.pprint(r)
-print("##################")
-# print("JSON:\n{0}".format(r))
-# print("##################")
+print("##################\nLinter debug = False")
+
 
 try:
-    r_lint = d.lintDockerFile(filename, rulesfile)
+    r_lint = d.lintDockerFile(dockerfile, rulesfile, volume, False)
     print(r_lint)
+    
 except Exception as ex:
     print(ex)
+    retcode = 1
+
+print("##################\nLinter debug = True")   
+
+try:    
+    r_lint_debug = d.lintDockerFile(dockerfile, rulesfile, volume, debug)
+    print(r_lint_debug)
+except Exception as ex:
+    print(ex)
+    retcode = 1
     
-exit(0)
+exit(retcode)
