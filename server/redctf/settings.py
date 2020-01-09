f26b4862309f4c33d4d71432cf05eb6c9bf24eaa
jrue3084 <jrue3084@gmail.com>
2020-01-09 04:18:30 +0000

    added upload to challenge model

--- server/redctf/settings.py
+++ server/redctf/settings.py
@@ -129,6 +129,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+# MEDIA_ROOT for storing model file uploads
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
