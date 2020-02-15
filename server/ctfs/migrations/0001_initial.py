348d558df9667c23df0a03545164ba8e466add1e
jrue3084 <jrue3084@gmail.com>
2020-02-15 22:00:43 +0000

    recommit initial migrations

---
+++ server/ctfs/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Ctf',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('start', models.DateTimeField()),
+                ('end', models.DateTimeField()),
+                ('created', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
