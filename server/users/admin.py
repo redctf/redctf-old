7151478d56a996c9140ee40b1575513ba5f9cc87
Jeris Rue <jeris.rue@gmail.com>
2022-01-21 14:43:24 -0600

    added graphql_auth models

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
+from django.apps import apps
 
 admin.site.register(User)
+
+app = apps.get_app_config('graphql_auth')
+
+for model_name, model in app.models.items():
+    admin.site.register(model)
